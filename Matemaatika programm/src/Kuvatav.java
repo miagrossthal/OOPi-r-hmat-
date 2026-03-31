5d5c3227697bac1fb690850c747c96358aafd823
joosepkallaste <joosep.kallaste@gmail.com>
2026-03-31 21:06:35 +0300

    Delete Matemaatika programm directory

--- Matemaatika programm/src/Kuvatav.java
+++
@@ -1,8 +0,0 @@
-/**
- * Kuvatav on liides objektidele, mida saab ekraanile kuvada.
- * See on vajalik selleks, et erinevaid objekte saaks ühtemoodi kuvada.
- */
-
-public interface Kuvatav {
-    void kuva();
-}
